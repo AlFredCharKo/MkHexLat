cdede27e01119aeded797c5622d2f1e9ae3ac38e
Alexander Kompch <alexander.kompch@web.de>
2016-02-24 18:12:44 +0100

    Initial Commit

---
+++ MkHexLat/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MkHexLat
+//
+//  Created by Alexander Kompch on 24.02.16.
+//  Copyright (c) 2016 Alexander Kompch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
